9a6449b387d18da235a38af43ed5766d3b43fcee
Jeremy Soto <99754887+soto503@users.noreply.github.com>
2024-12-07 22:33:28 -0600

    Update SQL2HTML.py

--- SQL2HTML.py
+++ SQL2HTML.py
@@ -4,7 +4,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host='*******',
         user='*****',
-        password='*****!',
+        password='******',
         port='****',
         database='******'
     )
