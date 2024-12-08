7f7a138add17c483120b22e0bf8e46a75f684001
Jeremy Soto <99754887+soto503@users.noreply.github.com>
2024-12-07 22:34:12 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_default_secret_key'  # Change this to a strong secret key
-    MYSQL_HOST = os.environ.get('MYSQL_HOST') or 'swoldier.cv0qs42yqp2y.us-east-2.rds.amazonaws.com'
-    MYSQL_USER = os.environ.get('MYSQL_USER') or 'cyberdudes'
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or 'Cybi123!'
-    MYSQL_DATABASE = os.environ.get('MYSQL_DATABASE') or 'swoldier'
-    MYSQL_PORT = os.environ.get('MYSQL_PORT') or '3306'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or '*********'  # Change this to a strong secret key
+    MYSQL_HOST = os.environ.get('MYSQL_HOST') or '*********'
+    MYSQL_USER = os.environ.get('MYSQL_USER') or '******'
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or '*******'
+    MYSQL_DATABASE = os.environ.get('MYSQL_DATABASE') or '*******'
+    MYSQL_PORT = os.environ.get('MYSQL_PORT') or '*******'
 
     @staticmethod
     def init_app(app):
